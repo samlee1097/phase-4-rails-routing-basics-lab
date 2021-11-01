39027b782be6615c0840b81d99fcc61d8f66d003
Samuel <samlee1097@yahoo.com>
2021-11-01 13:24:17 -0400

    commit tree 1b445b442223977697ccc5ecb421e7af3fc3266c

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,16 @@
+class StudentsController < ApplicationController
+    def index
+        students=Student.all
+        render json: students 
+    end
+
+    def grades
+        students=Student.all.order(grade: :desc)
+        render json: students 
+    end
+
+    def highest_grade
+        students=Student.all.order(grade: :desc)[0]
+        render json: students 
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "students/grades", to: "students#grades"
+  get "students/highest-grade", to: "students#highest_grade"
+  get "students", to: "students#index"
 end
